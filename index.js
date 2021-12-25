c04b7319e9a8381f0764d5ccc889c620efb27a5c
Rak355 <rakeshlal.591c@gmail.com>
2021-12-25 16:04:14 +0530

    route testing

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 var app = express();
 var http = require('http');
 
-app.use('/', function(req,res){
-    res.send("welcome")
-});
-
 app.use('/register', function(req,res){
     res.send("please register")
 });
 
-//.listen(process.env.PORT);
-app.listen(process.env.PORT);
+app.use('/login', function(req, res){
+    res.send("please login")
+});
+
+app.use('/', function(req,res){
+    res.send("welcome")
+});
+
+//app.listen(process.env.PORT);
+app.listen(5000);
