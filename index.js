f7a69e768980570c6b22af454e1873977d477e13
Rak355 <rakeshlal.591c@gmail.com>
2021-12-25 15:17:58 +0530

    port corrected

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.get('/register', function(req,res){
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080);
+}).listen(process.env.PORT);
